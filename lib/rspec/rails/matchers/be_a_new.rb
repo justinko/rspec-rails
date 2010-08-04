98be06d98acb81f9d9468dc2b70c1500bd5982a6
justinko <jko170@gmail.com>
2010-08-04 01:59:16 -0400

    Implementation for issue #131

--- lib/rspec/rails/matchers/be_a_new.rb
+++ lib/rspec/rails/matchers/be_a_new.rb
@@ -1,5 +1,21 @@
 RSpec::Matchers.define :be_a_new do |model_klass|
+  def with(attributes)
+    unless attributes.is_a?(Hash)
+      raise ArgumentError.new("#{attributes.inspect} must be a Hash")
+    end
+    @attributes = attributes.stringify_keys.to_a
+    self
+  end
+  
   match do |actual|
-    model_klass === actual && actual.new_record?
+    is_model_klass_new_record = model_klass === actual && actual.new_record?
+    
+    if @attributes
+      @attributes.all? do |key, value|
+        actual.attributes[key].eql?(value)
+      end and is_model_klass_new_record
+    else
+      is_model_klass_new_record
+    end
   end
 end
--- spec/rspec/rails/matchers/be_a_new_spec.rb
+++ spec/rspec/rails/matchers/be_a_new_spec.rb
@@ -18,7 +18,7 @@ describe "be_a_new matcher" do
       end
     end
   end
-
+  
   context "existing record" do
     let(:record) do
       Class.new do
@@ -33,6 +33,46 @@ describe "be_a_new matcher" do
     context "wrong class" do
       it "fails" do
         record.should_not be_a_new(String)
+      end
+    end
+  end
+  
+  describe "#with" do
+    let(:record) do
+      class WidgetFoo < ActiveRecord::Base
+        establish_connection :adapter => 'sqlite3',
+                             :database => ':memory:'
+
+        connection.execute <<-eosql
+          CREATE TABLE widget_foos (
+            foo_id integer,
+            number integer
+          )
+        eosql
+      end; WidgetFoo.new(:foo_id => 1, :number => 1)
+    end
+        
+    context "all attributes same" do
+      it "passes" do
+        record.should be_a_new(record.class).with(:foo_id => 1, :number => 1)
+      end
+    end
+    
+    context "one attribute as same" do
+      it "passes" do
+        record.should be_a_new(record.class).with(:foo_id => 1)
+      end
+    end
+    
+    context "no attributes as same" do
+      it "fails" do
+        record.should_not be_a_new(record.class).with(:zoo => 'zoo', :car => 'car')
+      end
+    end
+    
+    context "one attribute value not the same" do
+      it "fails" do
+        record.should_not be_a_new(record.class).with(:foo_id => 2)
       end
     end
   end
