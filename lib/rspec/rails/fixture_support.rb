28d23863c22cb90c4c2dcb96b06fe9842197e35d
justinko <jko170@gmail.com>
2010-08-22 03:23:11 -0400

    Move ActiveRecord::TestFixtures to outside of "included" block

--- lib/rspec/rails/fixture_support.rb
+++ lib/rspec/rails/fixture_support.rb
@@ -1,4 +1,13 @@
 if defined?(ActiveRecord)
+  
+  RSpec.configure do |c|
+    c.add_setting :use_transactional_fixtures
+    c.add_setting :use_transactional_examples, :alias => :use_transactional_fixtures
+    c.add_setting :use_instantiated_fixtures
+    c.add_setting :global_fixtures
+    c.add_setting :fixture_path
+  end
+  
   module RSpec
     module Rails
       module FixtureSupport
@@ -7,11 +16,11 @@ if defined?(ActiveRecord)
         include RSpec::Rails::SetupAndTeardownAdapter
         include RSpec::Rails::TestUnitAssertionAdapter
 
-        included do
-          if RSpec.configuration.use_transactional_fixtures
-            # TODO - figure out how to move this outside the included block
-            include ActiveRecord::TestFixtures
-
+        
+        if RSpec.configuration.use_transactional_fixtures
+          include ActiveRecord::TestFixtures
+          
+          included do
             self.fixture_path = RSpec.configuration.fixture_path
             self.use_transactional_fixtures = RSpec.configuration.use_transactional_fixtures
             self.use_instantiated_fixtures  = RSpec.configuration.use_instantiated_fixtures
@@ -21,13 +30,6 @@ if defined?(ActiveRecord)
       end
     end
   end
-
-  RSpec.configure do |c|
-    c.include RSpec::Rails::FixtureSupport
-    c.add_setting :use_transactional_fixtures
-    c.add_setting :use_transactional_examples, :alias => :use_transactional_fixtures
-    c.add_setting :use_instantiated_fixtures
-    c.add_setting :global_fixtures
-    c.add_setting :fixture_path
-  end
+  
+  RSpec.configure {|c| c.include RSpec::Rails::FixtureSupport }
 end
